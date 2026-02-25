324b4b70b21a6693b6d0826601d8384860b5da8c
l3k4n <alowoojalekan@gmail.com>
2026-02-25 16:15:53 -0600

    add biome, browserlist and lightningcss

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
+import browserslist from 'browserslist';
+import { browserslistToTargets } from 'lightningcss';
 import path from 'path'
 
 export default defineConfig({
   plugins: [solid()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      '@components': path.resolve(__dirname, './src/components'),
+      '@core': path.resolve(__dirname, './src/core'),
+      '@pages': path.resolve(__dirname, './src/pages'),
     },
   },
+  css: {
+    transformer: 'lightningcss',
+    lightningcss: {
+      targets: browserslistToTargets(browserslist('samsung >= 4'))
+    }
+  }
 })
