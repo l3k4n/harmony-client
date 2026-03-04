dcea9aa68d14ac0444fda5ce5899e3e1539bce50
l3k4n <alowoojalekan@gmail.com>
2026-03-04 08:21:59 -0600

    fix videojs error after navigation

--- src/pages/watch/index.tsx
+++ src/pages/watch/index.tsx
@@ -1,12 +1,14 @@
 import { onMount } from 'solid-js';
+import { useBeforeLeave } from '@solidjs/router';
 import videojs from 'video.js';
 import { videojs_config } from './videojsconfig';
 import './videojscomponents';
 import './style.css';
 
 export default function Watch() {
+	let player_ref: HTMLVideoElement | undefined;
 	onMount(() => {
-		const player = videojs('main-player', videojs_config);
+		const player = videojs(player_ref!, videojs_config);
 
 		player.loadMedia(
 			{
@@ -39,9 +41,29 @@ export default function Watch() {
 		);
 	});
 
+	useBeforeLeave(() => {
+		const player = videojs(player_ref!);
+		if (player) player.dispose();
+	});
+
 	return (
 		<section class="watch-page">
-			<video id="main-player" class="video-js vjs-skin" />
+			<video ref={player_ref} id="main-player" class="video-js vjs-skin" />
 		</section>
 	);
 }
+
+// function Watch() {
+// 	let player_ref: HTMLVideoElement | undefined;
+// 	onMount(() => {
+// 		// this throws err
+// 		const player = videojs('main-player', player_ref);
+//
+// 	});
+//
+// 	return (
+// 		<section class="watch-page">
+// 			<video ref={player_ref} id="main-player" class="video-js vjs-skin" />
+// 		</section>
+// 	);
+// }
